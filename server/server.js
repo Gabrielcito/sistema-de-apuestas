3f56443130fafe543963065b3cf267b1c09aaed8
Gabrielcito <gabriel.diazs@usm.cl>
2025-01-09 14:14:21 -0300

    client url

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post("/getCode", async (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://sistema-de-apuestas.vercel.app",
         methods: ["GET", "POST"]
     }
 });
